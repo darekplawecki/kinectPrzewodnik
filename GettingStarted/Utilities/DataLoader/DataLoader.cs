0553f39f031c54f8618b80e90ca09df003b47ba9
maciejek <radko.m@hotmail.com>
2013-11-28 16:20:58 +0100

    zaciskanie rączek i ładowanie wydarzeń

--- GettingStarted/Utilities/DataLoader/DataLoader.cs
+++ GettingStarted/Utilities/DataLoader/DataLoader.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
 using Przewodnik.Content.Traslations;
-using Przewodnik.Utilities.Twitter;
 
 namespace Przewodnik.Utilities.DataLoader
 {
@@ -26,21 +18,10 @@ namespace Przewodnik.Utilities.DataLoader
             LoadInstagram();
             LoadTwitter();
             LoadMaps();
-           // LoadCalendar();
+            LoadCalendar();
             LoadWeather();
             return true;
         }
-
-
-        //WYTYCZNE, CO DO ŁADOWANIA:
-        //- Tworzymy własną klasę i w niej dokonujemy ładowania.
-        //- Efekt wyjściowy ma nie wyrzucać wyjątków - jeżeli coś idzie nie tak, wyjątki obsługujemy
-        //  w własnym loaderze, a nie tej (dataLoader) klasie.
-        //- DataLoader przed wywolaniem jakiegokolwiek ladowania ponizej juz sprawdzil, czy uzytkownik
-        //  jest podlaczony do internetu. Jezeli nie jest, aplikacja w ogole sie nie uruchamia.
-        // W tej chwili wysypuje się błąd, ponieważ mój DataLoader jest taki głupi, albo ja, że nie potrafi wczytać pogody.
-        //  W tej chwili wysypuje sie blad, bo Jadzia wczytuje pogode, zanim wlaczy sie DataLoader.
-        //  Po przezuceniu pogody w odpowiednie miejsce bedzie dzialac.
 
         private void LoadInstagram()
         {
--- GettingStarted/Views/MapPage.xaml.cs
+++ GettingStarted/Views/MapPage.xaml.cs
@@ -19,11 +19,41 @@ namespace Przewodnik.Views
         public MapPage(KinectPageFactory pageFactory)
         {
             InitializeComponent();
+            KinectRegion.AddQueryInteractionStatusHandler(MasterBingMap, OnQuery);
             _pageFactory = pageFactory;
 
             pvm = new PlacesViewModel();
             mvm = new MapViewModel();
             MapGrid.DataContext = mvm;
+        }
+
+        //Variable to track GripInterationStatus
+        bool isGripinInteraction = false;
+
+        private void OnQuery(object sender, QueryInteractionStatusEventArgs handPointerEventArgs)
+        {
+
+            //If a grip detected change the cursor image to grip
+            if (handPointerEventArgs.HandPointer.HandEventType == HandEventType.Grip)
+            {
+                isGripinInteraction = true;
+                handPointerEventArgs.IsInGripInteraction = true;
+            }
+
+           //If Grip Release detected change the cursor image to open
+            else if (handPointerEventArgs.HandPointer.HandEventType == HandEventType.GripRelease)
+            {
+                isGripinInteraction = false;
+                handPointerEventArgs.IsInGripInteraction = false;
+            }
+
+            //If no change in state do not change the cursor
+            else if (handPointerEventArgs.HandPointer.HandEventType == HandEventType.None)
+            {
+                handPointerEventArgs.IsInGripInteraction = isGripinInteraction;
+            }
+
+            handPointerEventArgs.Handled = true;
         }
 
         public Grid GetView()
