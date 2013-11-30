dfff09c506f3196567ef8e502cadb6e80ae139e2
maciejek <radko.m@hotmail.com>
2013-11-30 19:06:12 +0100

    troche ruchu dla progress bara

--- GettingStarted/Utilities/DataLoader/CalendarLoader.cs
+++ GettingStarted/Utilities/DataLoader/CalendarLoader.cs
@@ -12,8 +12,26 @@ namespace Przewodnik.Utilities.DataLoader
     {
         private static CalendarLoader instance;
         public Hashtable WWWSourcesHashtable;
+        public EventHandler calendarEventLoaded;
 
         private CalendarLoader()
+        {
+
+        }
+
+        public static CalendarLoader Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CalendarLoader();
+                }
+                return instance;
+            }
+        }
+
+        public void LoadEvents()
         {
             CookieAwareWebClient client = new CookieAwareWebClient();
             WWWSourcesHashtable = new Hashtable();
@@ -26,26 +44,40 @@ namespace Przewodnik.Utilities.DataLoader
                 string parameter = dt.AddDays(i).ToString("yyyy-MM-dd");
                 string link = "http://pik.wroclaw.pl/d" + parameter;
                 var result = client.DownloadData(link);
-                var html = System.Text.Encoding.Default.GetString(result); 
+                var html = System.Text.Encoding.Default.GetString(result);
 
                 //string parameter = dt.AddDays(i).ToString("yyyy-MM-dd");
                 //StreamReader sr = new StreamReader("C:/Wydarzenie.txt");
                 //var html = sr.ReadToEnd();
                 //sr.Close();
-                               
+
                 WWWSourcesHashtable[parameter] = html;
+                moveProgress(i);
             }
         }
 
-        public static CalendarLoader Instance
+        private void moveProgress(int dayLoaded)
         {
-            get
+            switch (dayLoaded)
             {
-                if (instance == null)
-                {
-                    instance = new CalendarLoader();
-                }
-                return instance;
+                case 2:
+                    calendarEventLoaded(this, new AfterDataLoaderEventArgs(4, 10));
+                    break;
+                case 4:
+                    calendarEventLoaded(this, new AfterDataLoaderEventArgs(5, 10));
+                    break;
+                case 6:
+                    calendarEventLoaded(this, new AfterDataLoaderEventArgs(6, 10));
+                    break;
+                case 8:
+                    calendarEventLoaded(this, new AfterDataLoaderEventArgs(7, 10));
+                    break;
+                case 10:
+                    calendarEventLoaded(this, new AfterDataLoaderEventArgs(8, 10));
+                    break;
+                case 12:
+                    calendarEventLoaded(this, new AfterDataLoaderEventArgs(9, 10));
+                    break;
             }
         }
 
--- GettingStarted/Utilities/DataLoader/DataLoader.cs
+++ GettingStarted/Utilities/DataLoader/DataLoader.cs
@@ -18,37 +18,47 @@ namespace Przewodnik.Utilities.DataLoader
             LoadInstagram();
             LoadTwitter();
             LoadMaps();
-            //LoadCalendar();
+            LoadCalendar();
             LoadWeather();
             return true;
+        }
+
+        private void CalendarEventLoaded(object sender, EventArgs eventArgs)
+        {
+            if (eventArgs != null)
+            {
+                AfterDataLoaderEventArgs eventRetreived = eventArgs as AfterDataLoaderEventArgs;
+                afterLoaderEvents(this, new AfterDataLoaderEventArgs(eventRetreived.Many, eventRetreived.Of));
+            }
         }
 
         private void LoadInstagram()
         {
             beforeLoaderEvents(this, new BeforeDataLoaderEventArgs(AppResources.GetText("LOAD_instagram")));
             new InstagramLoader().LoadInstagram();
-            afterLoaderEvents(this, new AfterDataLoaderEventArgs(1, 5));
+            afterLoaderEvents(this, new AfterDataLoaderEventArgs(1, 10));
         }
 
         private void LoadTwitter()
         {
             beforeLoaderEvents(this, new BeforeDataLoaderEventArgs(AppResources.GetText("LOAD_twitter")));
             new TwitterLoader().LoadTwitter();
-            afterLoaderEvents(this, new AfterDataLoaderEventArgs(2, 5));
+            afterLoaderEvents(this, new AfterDataLoaderEventArgs(2, 10));
         }
 
         private void LoadMaps()
         {
             beforeLoaderEvents(this, new BeforeDataLoaderEventArgs(AppResources.GetText("LOAD_maps")));
             MapLoader ml = MapLoader.Instance;
-            afterLoaderEvents(this, new AfterDataLoaderEventArgs(3, 5));
+            afterLoaderEvents(this, new AfterDataLoaderEventArgs(3, 10));
         }
 
         private void LoadCalendar()
         {
             beforeLoaderEvents(this, new BeforeDataLoaderEventArgs(AppResources.GetText("LOAD_events")));
             CalendarLoader cl = CalendarLoader.Instance;
-            afterLoaderEvents(this, new AfterDataLoaderEventArgs(4, 5));
+            cl.calendarEventLoaded += CalendarEventLoaded;
+            cl.LoadEvents();
         }
 
         private void LoadWeather()
